f5c38dd18c3682751544f73bd3c8276b5ce6d1ac
Cris Daniluk <cdaniluk@rhythmictech.com>
2019-06-02 19:24:02 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,107 @@
+resource "aws_iam_role" "FullAdminAccess_role" {
+  name                 = "${var.role_prefix}FullAdminAccess"
+  max_session_duration = 7200
+  path                 = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "AWS": "arn:aws:iam::${var.master_account}:root"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "FullAdminAccess_role_policy_attach" {
+  role       = "${aws_iam_role.FullAdminAccess_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_role" "StandardAdminAccess_role" {
+  name                 = "${var.role_prefix}StandardAdminAccess"
+  max_session_duration = 7200
+  path                 = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "AWS": "arn:aws:iam::${var.master_account}:root"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "StandardAdminAccess_role_policy_attach" {
+  role       = "${aws_iam_role.StandardAdminAccess_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+}
+
+resource "aws_iam_role" "FullReadOnlyAccess_role" {
+  name                 = "${var.role_prefix}FullReadOnlyAccess"
+  max_session_duration = 7200
+  path                 = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "AWS": "arn:aws:iam::${var.master_account}:root"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "FullReadOnlyAccess_role_policy_attach" {
+  role       = "${aws_iam_role.FullReadOnlyAccess_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+resource "aws_iam_role" "SecurityAnalyst_role" {
+  name                 = "${var.role_prefix}SecurityAnalyst"
+  max_session_duration = 7200
+  path                 = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "AWS": "arn:aws:iam::${var.master_account}:root"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "SecurityAnalyst_role_policy_attach" {
+  role       = "${aws_iam_role.SecurityAnalyst_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type = "string"
+}
+
+variable "role_prefix" {
+  default = ""
+}
+
+variable "master_account" {
+  description = "account authenticating from"
+  type        = "string"
+}
